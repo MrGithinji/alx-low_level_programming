3b3bbd99ebc580934fe67d00c89872be0a5c665b
MrGithinji <githinjikevin@gmail.com>
2022-06-02 18:09:36 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Prints a message
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
