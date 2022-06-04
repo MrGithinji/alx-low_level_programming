eafba675d29391a3bb061bdad17fc55af489bfe2
MrGithinji <githinjikevin@gmail.com>
2022-06-04 06:31:39 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Prints alphabet in lower case
+*
+* Return: 0
+*/
+int main(void)
+{
+	char ch = 'abcdefghijklmnopqrstuvwxyz';
+
+	putchar(ch);
+	return (0);
+}
