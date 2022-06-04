c4959c87087c70a9dc4c6627b2addfb8ba3919c2
MrGithinji <githinjikevin@gmail.com>
2022-06-04 09:01:32 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main -  Prints 0 to 9
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+
+	putchar('\n');
+	return (0);
+}
