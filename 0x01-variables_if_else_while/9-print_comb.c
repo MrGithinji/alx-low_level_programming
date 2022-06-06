892ef047629193e712c29ea1a1e8a818fe5ee959
MrGithinji <githinjikevin@gmail.com>
2022-06-06 09:13:00 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main -  Prints 0 to 9
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+		putchar(', ')
+	}
+	putchar('\n');
+	return (0);
+}
