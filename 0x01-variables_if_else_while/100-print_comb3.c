009f6a222bdc34c82103fdfdf85d394a51379b21
MrGithinji <githinjikevin@gmail.com>
2022-06-06 09:55:52 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all combinations of two numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int dig1;
+	int dig2;
+
+	for (dig1 = 0; dig1 < 9; dig1++)
+	{
+		for (dig2 = dig1 + 1; dig2 < 10; dig2++)
+		{
+			putchar((dig1 % 10) + '0');
+			putchar((dig2 % 10) + '0');
+
+			if (dig1 == 8 && dig2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
