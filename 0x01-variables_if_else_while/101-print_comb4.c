de409cb7b37949d3160eb2d022bbdd864cd3a5af
MrGithinji <githinjikevin@gmail.com>
2022-06-06 10:18:42 -0700

    task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,7 +16,7 @@ int main(void)
 	{
 		for (dig2 = dig1 + 1; dig2 < 10; dig2++)
 		{
-			for (dig3 = dig1 + 2; dig3 < 10; dig3++)
+			for (dig3 = dig2 + 1; dig3 < 10; dig3++)
 			{
 				putchar((dig1 % 10) + '0');
 				putchar((dig2 % 10) + '0');
