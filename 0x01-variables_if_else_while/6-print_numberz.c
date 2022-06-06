12bcaa4ba9445b44ea6cde98e85896c88834cc51
MrGithinji <githinjikevin@gmail.com>
2022-06-06 06:49:58 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main -  Prints 0 to 9
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
+
