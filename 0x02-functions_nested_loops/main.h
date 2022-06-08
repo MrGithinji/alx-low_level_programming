bbc26d9598a3c4ea988a281f9162410d614cf65e
MrGithinji <githinjikevin@gmail.com>
2022-06-08 09:45:38 -0700

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,2 @@
-#ifndef MAIN
-#define MAIN
-/*
- * File: main.h
- * Auth: Githinji githogora
- * Desc: Header file. has declarations foe functions
- *
- */
-
 int _putchar(char c);
 void print_alphabet(void);
-
-#endif
