0f94a7108bf70fb0ac7f36fa47db73619bf0fe9c
MrGithinji <githinjikevin@gmail.com>
2022-06-08 15:36:40 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lowercase letters
+ * @c: character being checked
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
