ad76e2a01c203ed2df72ea5b35ae81251bad1e38
MrGithinji <githinjikevin@gmail.com>
2022-06-13 10:23:31 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ *
+ * @str: string to be printed
+ */
+
+void _puts(char *str)
+{
+	printf('%d\n', str);
+}
