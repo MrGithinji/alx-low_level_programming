b1d5750bdc7dc34614b9d6bd6e7f6b9a73060e0e
MrGithinji <githinjikevin@gmail.com>
2022-06-13 09:52:06 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a - first int
+ * @b - second int
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = x;
+	*b = y;
+
+	*a = y;
+	*b = x;
+}
