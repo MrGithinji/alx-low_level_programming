31f76ea067dbd95c641112cdae00ea9e5f142e32
MrGithinji <githinjikevin@gmail.com>
2022-06-13 10:42:24 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,13 +3,19 @@
 #include <string.h>
 
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * puts - usage of puts() function
+ * _puts - prints a string, followed by a new line, to stdout 
  *
  * @str: string to be printed
  */
 
 void _puts(char *str)
 {
-	int puts(const char *str)
+	int i;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_purchar('\n');
 }
