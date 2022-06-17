0cec52f2acbe876e7a1bd5b2774a499b70fe6d4d
MrGithinji <githinjikevin@gmail.com>
2022-06-17 16:16:30 -0700

    task 2 retake

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,20 +4,27 @@
 
 /**
  * *_strncpy - copies a string
- * 
+ *
  * @dest: string 1
  * @src: string 2
- *
- * Return: dest 
+ * @n: max size of copy
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
 	return (dest);
 }
