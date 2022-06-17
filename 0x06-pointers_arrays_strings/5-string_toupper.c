f622f6b61d05d4535d3e9e8c28773924a6d0d318
MrGithinji <githinjikevin@gmail.com>
2022-06-17 05:29:46 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/**
+ * reverse_array - reverses an arry
+ *
+ * Return: 0
+ */
+
 char *string_toupper(char *)
 {
-	while(*[i])
+	while (*[i])
 	{
 		_putchar(toupper(str[i]));
 		i++;
 	}
-   
-   return (0);
+
+	return (0);
 }
