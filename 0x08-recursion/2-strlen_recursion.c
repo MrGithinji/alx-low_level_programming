f312d38586a2e7e53772a8202dfcf578fac2bf95
MrGithinji <githinjikevin@gmail.com>
2022-06-22 11:23:43 -0700

    task 1

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen_recursion - prints a string, in reverse.
+ *
+ * @s: string to be printed
+ */
+
+int _strlen_recursion(char *s);
+{
+	int j;
+	int k;
+
+	j = strlen(str);
+	k = j - 1;
+
+	while (str[k])
+	{
+		_putchar(str[k]);
+		k--;
+	}
+}
