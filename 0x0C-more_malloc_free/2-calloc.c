cbade11f10f206f5831c75d7f791830dfcadcc17
MrGithinji <githinjikevin@gmail.com>
2022-06-30 15:49:28 -0700

    task 1 corrections

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,41 +9,23 @@
  * @n: s2's number of bytes
  * Return: 0
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	char *ptr;
+	unsigned int count;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
 	{
-		arr[co] = s2[co_2];
-		co++;
+		return (NULL);
 	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	count = 0;
+	while (count < nmemb * size)
+	{
+		ptr[count] = 0;
+		count++;
+	}
+	return (ptr);
 }
