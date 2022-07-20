a18af3778137deb55d77859b2e3ded3e6d91afed
MrGithinji <githinjikevin@gmail.com>
2022-07-20 10:24:32 -0700

    lists file amendment

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,13 +10,14 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
+
 
 int _putchar(char c),
 
